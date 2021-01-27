410ba02df3deae0b40ca3d37d1bf1f572bfdc98c
zans.litinskis@gmail.com <zans.litinskis@gmail.com>
2021-01-27 20:35:08 +0000

    COMPOSTION VS INHERITANCE

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 
 import { MatchReader } from './MatchReader';
+import { CsvFileReader } from './CsvFileReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new MatchReader('football.csv');
+// Create an object that satisfies the Data reader interface
+const csvFileReader = new CsvFileReader('football.csv');
 
-reader.read();
+const matchReader = new MatchReader(csvFileReader);
+
+matchReader.load();
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
     if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
         manUnitedWins++;
     } else if(match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
